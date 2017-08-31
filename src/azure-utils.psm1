10ca84566ec3f0990c7f90bf94a3f34679a22e43
Maarten Kools <mkools@attune.gshapps.net>
2017-08-31 15:00:16 +0200

    Added a test whether AzureRM is installed
    If AzureRM is not installed, an installation is attempted.

--- src/azure-utils.psm1
+++ src/azure-utils.psm1
@@ -1,3 +1,12 @@
+Function Test-Elevation {
+    [Security.Principal.WindowsPrincipal] $Identity = [Security.Principal.WindowsIdentity]::GetCurrent()            
+    If (-not ($Identity.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+        Return $false
+    }
+
+    Return $true
+}
+
 Function Start-ResourceGroup {
     param(
         [Parameter(Mandatory = $true, Position = 1)]
@@ -10,6 +19,22 @@ Function Start-ResourceGroup {
     if (-not ($regex.IsMatch($GroupTier))) {
         Write-Host "The group tier is not the correct format. Please provide the group tier in g2xx format"
         return
+    }
+
+    Write-Host "Checking for AzureRM modules..."
+    if (-not (Get-Module -ListAvailable -Name "AzureRM*")) {
+        if (-not (Get-Module -ListAvailable -Name "PowerShellGet")) {
+            Write-Host "You need to have PowerShellGet installed. Please visit https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.3.1#how-to-get-powershellget"
+            return
+        }
+        
+        if (-not (Test-Elevation)) {
+            Write-Host "This script requires elevation. Please start a PowerShell session as an Administrator"
+            return
+        }
+
+        Install-Module AzureRM
+        Import-Module AzureRM
     }
 
     Write-Host "Starting resource group with tier $GroupTier..."
@@ -26,3 +51,5 @@ Function Start-ResourceGroup {
         Start-AzureRmVM -Name $name -ResourceGroupName $resourceGroupName
     }
 }
+
+Export-ModuleMember -Function Start-ResourceGroup
