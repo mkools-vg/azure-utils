9b980c31667e05a0a60cd31a854b43889fae2d40
Maarten Kools <mkools@attune.gshapps.net>
2017-09-28 14:24:37 +0200

    Changed the argument to the resource group name
    It's no longer possible to determine the resource group name based on a
    group tier, so instead of specifying just a group tier, you need to
    specify a complete resource group name.

--- src/azure-utils.psm1
+++ src/azure-utils.psm1
@@ -31,16 +31,8 @@ Function Invoke-Login {
 Function Start-ResourceGroup {
     param(
         [Parameter(Mandatory = $true, Position = 1)]
-        [string]$GroupTier
+        [string]$ResourceGroupName
     )
-
-    $regex = New-Object "System.Text.RegularExpressions.Regex" `
-        -ArgumentList @("^g2\d{2}$", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
-
-    if (-not ($regex.IsMatch($GroupTier))) {
-        Write-Host "The group tier is not the correct format. Please provide the group tier in g2xx format"
-        return
-    }
 
     Write-Host "Checking for AzureRM modules..."
     if (-not (Get-Module -ListAvailable -Name "AzureRM*")) {
@@ -58,7 +50,7 @@ Function Start-ResourceGroup {
         Import-Module AzureRM
     }
 
-    Write-Host "Starting resource group with tier $GroupTier..."
+    Write-Host "Starting resource group $ResourceGroupName..."
 
     $profile = Join-Path $env:LOCALAPPDATA -ChildPath "azure-utils" | Join-Path -ChildPath "profile.json"
     if (-not (Test-Path $profile)) {
@@ -78,12 +70,11 @@ Function Start-ResourceGroup {
 
     Select-AzureRmSubscription -SubscriptionName "Non-Production"
 
-    $resourceGroupName = "lab17$GroupTier-rg"
-    Get-AzureRmVM -ResourceGroupName $resourceGroupName | ForEach-Object {
+    Get-AzureRmVM -ResourceGroupName $ResourceGroupName | ForEach-Object {
         $name = $_.Name
         Write-Host "  Starting $name..."
 
-        Start-AzureRmVM -Name $name -ResourceGroupName $resourceGroupName
+        Start-AzureRmVM -Name $name -ResourceGroupName $ResourceGroupName
     }
 }
 
